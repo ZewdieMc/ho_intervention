fix arm jacobian

scripts/swiftpro_manipulator.py
@@ -31,9 +31,7 @@ class SwiftProManipulator:
         self.dof = 4
         self.q = np.zeros(self.dof).reshape(-1,1)
         self.update_kinematics()
-
-
-       
+               
 
         # Subscriber
         self.joint_state_sub = rospy.Subscriber("/turtlebot/joint_states", JointState, self.joint_state_callback)
@@ -137,7 +135,7 @@ class SwiftProManipulator:
         ee_position = np.array([self.x_ee, self.y_ee, self.z_ee,0, 0, self.yaw_ee]).reshape(-1,1)
         return ee_position
     
-    '''
+    '''self.T
         Method that returns the position of a selected joint.
         Argument:
         joint (integer): index of the joint

scripts/task_priority.py
@@ -14,8 +14,8 @@ class TaskPriority:
         P = np.eye(robot.dof)
         dq = np.zeros(robot.dof).reshape(-1,1)
         W=np.eye(robot.dof)
-        W[0, 0] = 10
-        W[1, 1] = 10
+        W[0, 0] = 1
+        W[1, 1] = 1
         for i, task in enumerate(self.tasks):
             task.update(robot)
             # if task.name == "Position":
@@ -23,12 +23,11 @@ class TaskPriority:
             self.publish_sigmad(sigmad)
             if task.isActive():
                 J = task.getJacobian()  # Task Jacobian
-
                 Ji_q = J @ P            # Augmented Jacobian
 
                 #compute task velocity
                 print("task.getGain(): ", task.getGain().shape)
-                xi_bar = task.getGain()*0.4 @ task.getError()
+                xi_bar = task.getGain()*0.7 @ task.getError()
                 print("error: ", xi_bar)
                 # print("task name: ", task.name)
 
@@ -44,6 +43,7 @@ class TaskPriority:
                 P = P - np.linalg.pinv(Ji_q) @ J
                 if self.goal_reached(xi_bar):
                     dq = np.zeros(robot.dof).reshape(-1,1)
+        print(dq)
 
         return dq       
     
@@ -60,9 +60,9 @@ class TaskPriority:
         marker.pose.position.x = msg[0]
         marker.pose.position.y = msg[1]
         marker.pose.position.z = msg[2]
-        marker.scale.x = 0.3
-        marker.scale.y = 0.3
-        marker.scale.z = 0.3
+        marker.scale.x = 0.05
+        marker.scale.y = 0.05
+        marker.scale.z = 0.05
         marker.color.g = 1.0
         marker.color.r = 0.0
         marker.color.a = 0.5

scripts/tp_controller_node.py
@@ -11,7 +11,8 @@ from ho_intervention.srv import PoseGoal, PoseGoalResponse
 from tf.transformations import euler_from_quaternion
 from std_msgs.msg import Bool
 from turtlebot_manipulator import TurtlebotManipulator
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PoseStamped
+from nav_msgs.msg import Odometry
 
 
 
@@ -41,6 +42,7 @@ class TPController:
 
         # base cmd_vel publisher
         self.base_cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        self.ee_pose_pub = rospy.Publisher("~ee_pose", Odometry, queue_size=10)
         # Sevice
         self.sp_mg_srv = rospy.Service('/swiftpro/move_to_goal', PoseGoal, self.handle_swiftpro_move_to_goal)
         
@@ -104,6 +106,9 @@ class TPController:
             # cmd.linear.x = min(max(dq[1], -0.5), 0.5)
             # cmd.angular.z = min(max(dq[0], -0.3), 0.3)
             self.base_cmd_vel_pub.publish(cmd)
+
+            ee_pose = self.robot.getEEPose()
+            self.publishEEpose(ee_pose[:3,0])
             if (rospy.Time.now() - start_time).to_sec() > self.time_limit:
                 res.success = False
                 self.stop_arm()
@@ -131,6 +136,18 @@ class TPController:
         sigma = self.robot.getEEPose()
         #print(np.linalg.norm(sigma - sigma_d))
         return np.linalg.norm(sigma - sigma_d) < 0.004
+    
+    def publishEEpose(self, ee):
+        """
+        Publishes the end-effector pose
+        """
+        ee_pose = Odometry()
+        ee_pose.header.stamp = rospy.Time.now()
+        ee_pose.header.frame_id = "world_ned"
+        ee_pose.pose.pose.position.x = ee[0]
+        ee_pose.pose.pose.position.y = ee[1]
+        ee_pose.pose.pose.position.z = ee[2]
+        self.ee_pose_pub.publish(ee_pose)
 
 if __name__ == '__main__':
 
@@ -138,11 +155,11 @@ if __name__ == '__main__':
     TP = TaskPriority(
             [
 
-                # JointLimitTask("Joint limit", np.array([0.05, 0.09]), np.array([-np.pi/2, np.pi/2]), 0),
-                JointLimitTask("Joint limit", np.array([-0.5, 2.5]), np.array([-0.5, 0.5]), 1),
-                # JointLimitTask("Joint limit", np.array([0.05, 0.09]), np.array([-np.pi/3, 0.05]), 2),
+                JointLimitTask("Joint limit", np.array([0.05, 0.09]), np.array([-np.pi/2, np.pi/2]), 0),
+                JointLimitTask("Joint limit", np.array([0.05, 0.05]), np.array([-np.pi/2, 0.05]), 1),
+                JointLimitTask("Joint limit", np.array([0.05, 0.09]), np.array([-np.pi/2, 0.05]), 2),
                 # JointLimitTask("Joint limit", np.array([0.05, 0.09]), np.array([-np.pi/2, np.pi/2]), 3),
-                PositionTask("Position", np.array([1.0, 1.0, -0.2]).reshape(-1,1)),
+                PositionTask("Position", np.array([1.0, -2.0, -0.1]).reshape(-1,1)),
             ]
         )
     robot = TPController(TP)

scripts/turtlebot_manipulator.py
@@ -89,7 +89,7 @@ class TurtlebotManipulator:
     Get the end-effector Jacobian.
     '''
     def getEEJacobian(self):
-        Jarm = self.arm.getEEJacobian()
+        Jarm = self.getArmJacobian()
         # T = self.T
         # T.append(self.T[-1] @ self.arm.T)
         # Jbase = jacobian(T, self.revolute)
@@ -117,3 +117,29 @@ class TurtlebotManipulator:
         return self.dof
  
         
+    def getArmJacobian(self):
+        J = np.zeros((6,self.arm.dof))
+        
+        q1,q2,q3,q4 = self.arm.q
+
+        l1p = -self.arm.l1*math.sin(q2)
+        l2p = self.arm.l2*math.cos(q3)
+        A = self.arm.lee + l1p + l2p + self.arm.lb
+        
+
+        dx_dq1 = -A * math.sin(q1)*math.sin(self.eta[3]) + A * math.cos(q1)*math.cos(self.eta[3])
+        dy_dq1 = A * math.sin(q1)*math.cos(self.eta[3]) + A * math.cos(q1)*math.sin(self.eta[3])
+
+        dx_dq2 = -self.arm.l1*math.cos(q2)*math.cos(q1)*math.sin(self.eta[3]) + (-self.arm.l1*math.cos(q2)*math.sin(q1))*math.cos(self.eta[3])
+        dy_dq2 = -self.arm.l1*math.cos(q2)*math.cos(q1)*-math.cos(self.eta[3]) + (-self.arm.l1*math.cos(q2)*math.sin(q1))*math.sin(self.eta[3])
+        dz_dq2 = self.arm.l1*math.sin(q2)
+
+        dx_dq3 = -self.arm.l2*math.sin(q3)*math.cos(q1)*math.sin(self.eta[3]) + (-self.arm.l2*math.sin(q3)*math.sin(q1))*math.cos(self.eta[3])
+        dy_dq3 = -self.arm.l2*math.sin(q3)*math.cos(q1)*-math.cos(self.eta[3]) + (-self.arm.l2*math.sin(q3)*math.sin(q1))*math.sin(self.eta[3])
+        dz_dq3 = -self.arm.l2*math.cos(q3)
+
+        J[:,0] = np.array([dx_dq1, dy_dq1,      0, 0, 0, 1 ])# derivertive by q1
+        J[:,1] = np.array([dx_dq2, dy_dq2, dz_dq2, 0, 0, 0 ]) # derivertive by q2
+        J[:,2] = np.array([dx_dq3, dy_dq3, dz_dq3, 0, 0, 0 ]) # derivertive by q3
+        J[:,3] = np.array([0, 0, 0, 0, 0, 1 ]) # derivertive by q4
+        return J